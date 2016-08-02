edd7c1e2ac779937cefd6dc6c878f1f71cc314de
Adnan Ostrakovic <oshtrak@gmail.com>
2016-08-02 12:46:19 +0200

    1

---
+++ src/Day2/Fajl.java
@@ -0,0 +1,45 @@
+/***
+ * 3. Napišite program koji ce brojati broj karaktera, rijeci i linija teksta u nekom fileu.
+ *  Rijeci trebaju biti odvojene jednim spaceom. Ime file proslijediti kao argument u vaš program.
+ */
+package Day2;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Fajl {
+
+	public static void main(String[] args) {
+		int bl=0;
+		int bk=0;
+		int br=0;
+		Scanner unos=new Scanner (System.in);
+		System.out.print("Unesite ime fajla: ");
+		File file= new File(unos.nextLine());
+		
+		try {
+		     BufferedReader buffer = new BufferedReader(new FileReader(file));
+		        String line;
+		        while ((line = buffer.readLine()) != null) {
+		            line = line.trim();
+		            bl++;
+		            for(int i=0;i<line.length();i++){
+		            	if(line.charAt(i)==' ') br++;
+		            	else bk++;
+		            	}
+		            br++;
+		            }
+						
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Vas fajl ima "+bk+" karaktera, "+br+" rijeci i "+bl+" linija");
+
+	}
+
+}
